2f6e58a217a5f61c251d11119ec0135af3943d38
oleksandrka <oleksandrka@KIE-X-L-0442.pt.playtech.corp>
2021-10-28 10:53:58 +0300

    using ami autosearch to create servers

--- 8_lesson/main.tf
+++ 8_lesson/main.tf
@@ -37,3 +37,61 @@ data "aws_ami" "latest_windows_server" {
     values = ["Windows_Server-2019-English-Full-Base-*"]
   }
 }
+
+# use lateste ami to create 
+resource "aws_instance" "amazon_linux_2_ami" {
+  ami                         = data.aws_ami.latest_amazon_linux.id
+  instance_type               = "t2.micro"
+  associate_public_ip_address = true
+  vpc_security_group_ids      = [aws_security_group.tf_mixpanel_test.id]
+
+  tags = {
+    Name  = "amazon linux 2"
+    Owner = "o.kashcheiev"
+  }
+}
+
+# the same with Windows
+resource "aws_instance" "windows_server" {
+  ami                         = data.aws_ami.latest_windows_server.id
+  instance_type               = "t2.micro"
+  associate_public_ip_address = true
+  vpc_security_group_ids      = [aws_security_group.tf_mixpanel_test.id]
+
+
+  tags = {
+    Name  = "amazon windows"
+    Owner = "o.kashcheiev"
+  }
+}
+
+
+
+resource "aws_security_group" "tf_mixpanel_test" {
+  name        = "terraform_mixpanel_tetsing"
+  description = "Allow ssh connection only"
+
+  dynamic "ingress" {
+    for_each = ["22"]
+    content {
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+
+  # allow all outgoing traffic
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name  = "allow_ssh_only"
+    Goal  = "test purpose"
+    Owner = "o.kashcheiev"
+  }
+}
